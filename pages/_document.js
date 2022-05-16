707a9ded895f97a2de845def98dbb3d481614a8e
Arpitnolkha <105585394+Arpitnolkha@users.noreply.github.com>
2022-05-16 18:25:03 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,17 +10,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="robots" content="follow, index" />
-        <meta name="description" content={meta.description} />
-        <meta property="og:site_name" content={meta.title} />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
+        Welcome to the land of fire fighters.
       </Head>
       <body>
         <Main />
