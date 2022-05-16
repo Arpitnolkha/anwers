3935bfa56d1ea19c66497d43983aabfdcb3fed09
Arpitnolkha <105585394+Arpitnolkha@users.noreply.github.com>
2022-05-16 17:29:48 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,10 +10,21 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-       Hey Welcome to my site, you can find my answers here.
+        <meta name="robots" content="follow, index" />
+        <meta name="description" content={meta.description} />
+        <meta property="og:site_name" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
       </Head>
       <body>
-        This Arpit and I love to be a support agent.
+        <Main />
+        <NextScript />
       </body>
     </Html>
   )
