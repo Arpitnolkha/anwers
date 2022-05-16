9727751b22b78a30ba1dd79db668450bdbdd3536
Arpitnolkha <105585394+Arpitnolkha@users.noreply.github.com>
2022-05-16 18:29:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 module.exports = withNextra()
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/Q3',
-        destination: 'https://vercel.com',
-        permanent: false,
-      },
-    ]
-  },
-}
+
